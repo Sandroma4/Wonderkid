845cc804b33c0ff7495c65d8612458a8cc9d0e60
Sandroma4 <romain.sandri@hotmail.com>
2026-08-20 02:17:38 +0200

    UI: Centrage absolu du conteneur de menu avec hauteur minimum fixe

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -366,7 +366,7 @@ export const MainMenu = ({ onNavigate, onLoadGame, onJoinInvite }) => {
         </div>
 
         {/* Menu Buttons */}
-        <div className="w-full space-y-4 animate-[slide-up_0.8s_ease-out]">
+        <div className="w-full min-h-[480px] flex flex-col justify-start space-y-4 animate-[slide-up_0.8s_ease-out]">
           
           {showPlayOptions ? (
             <div className="space-y-4 animate-[fade-in_0.3s_ease-out]">
